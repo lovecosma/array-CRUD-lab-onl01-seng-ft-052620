98578cedb86fddd66b2d592e0de1ae5f5939bb40
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 21:51:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ end
 def add_element_to_start_of_array(array, element)
 @d = array
 @element = element
-return @c.unshift(@element)
+return @d.unshift(@element)
 end
 
 def remove_element_from_end_of_array(array)
